81d741889cae9af6082f6e386d9080c828b8f847
Midhun Nath K R <midhunnathkr@gmail.com>
2025-08-29 12:58:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -106,6 +107,9 @@ app.get('/bfhl', (req, res) => {
         endpoint: "/bfhl",
         method: "POST"
     });
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start server
